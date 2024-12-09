e44270bece65862938b67d0dcc40f9c0a237a971
Federissis333 <110787619+Federissis333@users.noreply.github.com>
2024-12-09 16:44:19 -0300

    Add files via upload

---
+++ Creditos.js
@@ -0,0 +1,9 @@
+Desenvolvido Por: Nery #Programador
+Usuário do Discord: @nerydev
+
+bot oficial do servidor de vazamentos
+Vanguard: https://discord.gg/6WD79uahww
+
+por favor deixe este crédito é muito importante para mim
+se quiser uma versão sem créditos originais
+faça você mesmo ;)
